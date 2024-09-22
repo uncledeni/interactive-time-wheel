599f692d5d2c48a92d13af33b3da7ff817a3083f
BarDen <uncledeni@mail.ru>
2024-09-22 23:51:41 +0300

    Base project settings with webpack

---
+++ src/app.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const App = () => {
+    const title = 'React с Webpack и Babel';
+
+    return (
+        <div>
+            <h1>{title}</h1>
+        </div>
+    );
+}
---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const path = require('path');
+
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+// const webpack = require('webpack');
+
+module.exports = {
+    mode: "development",
+
+    devServer: {
+        historyApiFallback: true,
+        static: {
+            directory: path.resolve(__dirname, './dist')
+        },
+        open: true,
+        compress: true,
+        hot: true,
+        port: 8080,
+    },
+
+    entry: {
+        main: path.resolve(__dirname, './src/index.jsx')
+    },
+
+    output: {
+        path: path.resolve(__dirname, '/dist'),
+        filename: '[name].[hash].js'
+    },
+
+    plugins: [
+        new HTMLWebpackPlugin({ template: "./src/index.html" }),
+        new CleanWebpackPlugin()
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                // Сообщаем вебпаку, что для работы с js-, jsx-файлами
+                // следует использовать babel-loader
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                // Сообщаем вебпаку, что для работы с js-, jsx-файлами
+                // следует использовать babel-loader
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-react', '@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+        ],
+    },
+
+}
