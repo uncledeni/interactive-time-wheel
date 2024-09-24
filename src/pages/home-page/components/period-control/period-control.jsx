8c7893647a1ce7ee328806fdb95b4e2c9fe27438
Baranetcky_DA <uncledeni@mail.ru>
2024-09-24 15:01:00 +0300

    Start making circle rotation animation

--- src/pages/home-page/components/period-control/period-control.jsx
+++ src/pages/home-page/components/period-control/period-control.jsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
+const numIcons = [1, 2, 3, 4, 5, 6];
 
 const PeriodCircle = styled.div`
-    width: 536px;
+    width: 530px;
     height: 530px;
     position: absolute;
     top: 19%;
@@ -28,10 +32,65 @@ const YearsPeriod = styled.p`
     text-align: center;
 `
 
-export const PeriodControl = ({years}) => {
+export const PeriodControl = ({ years }) => {
+    const elemRef = useRef(null);
+    const circleRef = useRef(null);
+    const activePosition = numIcons.length;
+    const [offset, setOffset] = useState(0);
+
+    useEffect(() => {
+        // alert(offset)
+    }, [offset])
+
+    const TimePoint = styled.button`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 56px; height: 56px;
+    border: 1px solid #303E5880;
+    border-radius: 28px;
+    position: absolute; top: 50%; left: 50%;
+    cursor: pointer;
+    transform-origin: 0 0;
+    // scale: .2;
+    transform: translate(-50%, -50%) rotate(calc(${360 / numIcons.length}deg * var(--i))) translate(265px) rotate(calc(${-360 / numIcons.length}deg * var(--i)));
+
+    ${numIcons.map((e, i) => { return `&:nth-child(${i + 1}) { --i: ${i + offset};}` })}
+
+    & p {
+        font-family: PT Sans;
+        color: red;
+    }
+
+    &: {
+
+    }
+`
+
+    useGSAP(() => {
+        gsap.to(circleRef.current, {
+            duration: 2,
+            // repeat: -1,
+            rotation: 60
+        })
+    })
+
+    useGSAP(() => {
+        gsap.to(elemRef.current, {
+            duration: 2,
+            // repeat: -1,
+            rotation: 360
+        })
+    })
+
     return (
         <PeriodCircle>
-            <YearsPeriod>{`${years[0]} ${years[1]}`}</YearsPeriod>
+            {numIcons.map((e, i) => {
+                return <TimePoint key={e} onClick={() => { setOffset(i + 5) }}>
+                    <p>{i} - {e}</p>
+                </TimePoint>
+            })}
+            {/* <YearsPeriod>{`${years[0]} ${years[1]}`}</YearsPeriod> */}
         </PeriodCircle>
     )
 }
