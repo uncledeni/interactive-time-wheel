4eec88bf7d168133b74fabc4b544254334049c43
Baranetcky_DA <uncledeni@mail.ru>
2024-09-25 17:35:51 +0300

    Update circle animation

--- src/pages/home-page/components/period-control/period-control.jsx
+++ src/pages/home-page/components/period-control/period-control.jsx
@@ -32,17 +32,7 @@ const YearsPeriod = styled.p`
     text-align: center;
 `
 
-export const PeriodControl = ({ years }) => {
-    const elemRef = useRef(null);
-    const circleRef = useRef(null);
-    const activePosition = numIcons.length;
-    const [offset, setOffset] = useState(0);
-
-    useEffect(() => {
-        // alert(offset)
-    }, [offset])
-
-    const TimePoint = styled.button`
+const TimePoint = styled.button`
     display: flex;
     justify-content: center;
     align-items: center;
@@ -55,8 +45,10 @@ export const PeriodControl = ({ years }) => {
     // scale: .2;
     transform: translate(-50%, -50%) rotate(calc(${360 / numIcons.length}deg * var(--i))) translate(265px) rotate(calc(${-360 / numIcons.length}deg * var(--i)));
 
-    ${numIcons.map((e, i) => { return `&:nth-child(${i + 1}) { --i: ${i + offset};}` })}
-
+    ${props => {
+        return `&:nth-child(${props.$tmp + 1}) { --i: ${props.$tmp - 1};}`
+    }}
+    
     & p {
         font-family: PT Sans;
         color: red;
@@ -66,29 +58,69 @@ export const PeriodControl = ({ years }) => {
 
     }
 `
+const InnerText = ({ e, i, activePosition, currentActive }) => {
+    const textRef = useRef(null);
+    console.log(textRef.current, i, activePosition - i)
 
-    useGSAP(() => {
-        gsap.to(circleRef.current, {
-            duration: 2,
-            // repeat: -1,
-            rotation: 60
-        })
-    })
-
-    useGSAP(() => {
-        gsap.to(elemRef.current, {
-            duration: 2,
-            // repeat: -1,
-            rotation: 360
-        })
+    gsap.to(textRef.current, {
+        duration: 2,
+        // repeat: -1,
+        rotation: -60 * (activePosition - currentActive + 1),
     })
 
     return (
-        <PeriodCircle>
+        <p ref={textRef}>{i} - {e}</p>
+    )
+}
+
+const Point = ({ e, i, offset, currentActive, activePosition, func }) => {
+    const elemRef = useRef(null);
+
+    gsap.to(elemRef.current, {
+        duration: 2,
+        // repeat: -1,
+        // scale: 5.6,
+        // xPercent: 25,
+        // yPercent: 25
+    })
+
+    return (
+        <TimePoint
+        ref={elemRef}
+            $tmp={i}
+            $offset={offset}
+            onClick={func}
+        >
+            <InnerText e={e} i={i} activePosition={activePosition} currentActive={currentActive} />
+        </TimePoint>
+    )
+}
+
+export const PeriodControl = ({ years }) => {
+    const circleRef = useRef(null);
+    const [activePosition, setActivePosition] = useState(numIcons.length - 1);
+    const [currentActive, setCurrentActive] = useState(0);
+    const [offset, setOffset] = useState(0);
+
+    useEffect(() => {
+        // alert(offset)
+    }, [offset])
+
+
+    return (
+        <PeriodCircle ref={circleRef}>
             {numIcons.map((e, i) => {
-                return <TimePoint key={e} onClick={() => { setOffset(i + 5) }}>
-                    <p>{i} - {e}</p>
-                </TimePoint>
+                return <Point key={e} e={e} i={i} activePosition={activePosition} currentActive={currentActive} func={() => {
+                        if (i !== currentActive) {
+                            setOffset(Math.abs(activePosition - i + 1));
+                            setCurrentActive(i);
+                            gsap.to(circleRef.current, {
+                                duration: 2,
+                                // repeat: -1,
+                                rotation: 60 * (activePosition - i + 1)
+                            })
+                        }
+                    }} />
             })}
             {/* <YearsPeriod>{`${years[0]} ${years[1]}`}</YearsPeriod> */}
         </PeriodCircle>
